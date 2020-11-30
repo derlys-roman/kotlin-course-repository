5861369726197fdaeaef5b50642b425d39c13938
Derlys <derlysr6@gmail.com>
2020-11-30 19:49:30 -0300

    add random number method and add to the collection

--- FirstProject/src/Piramide.kt
+++ FirstProject/src/Piramide.kt
@@ -1,12 +1,17 @@
+import kotlin.random.Random
+
 class Piramide {
-    private val collectionRandom = arrayListOf(
+    private val exampleCollection = arrayListOf(
             listOf(1, 3),
             listOf(3, 5),
             listOf("stranger"),
             listOf("null"))
 
     fun printedACollections() {
-        for (listOf in collectionRandom) {
+        var aleato = (-50..20).random()
+        exampleCollection.add(listOf(aleato))
+        for (listOf in exampleCollection) {
+            print(aleato)
             println(listOf)
         }
     }
