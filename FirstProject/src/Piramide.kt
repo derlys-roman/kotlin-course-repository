9064e13350ae091fa4a9ae22a503a7b4bca1369c
Derlys <derlysr6@gmail.com>
2020-12-12 04:31:29 -0300

    I not know what modified now

--- FirstProject/src/Piramide.kt
+++ FirstProject/src/Piramide.kt
@@ -1,18 +1,10 @@
-import kotlin.random.Random
-
 class Piramide {
     private val exampleCollection = arrayListOf(
             listOf(1, 3),
             listOf(3, 5),
             listOf("stranger"),
             listOf("null"))
-
     fun printedACollections() {
-        var aleato = (-50..20).random()
-        exampleCollection.add(listOf(aleato))
-        for (listOf in exampleCollection) {
-            print(aleato)
-            println(listOf)
-        }
+        for (listOf in exampleCollection) println(listOf)
     }
 }
