049ead22b8907a9f52b0dea91a7e2a9775a8998e
Derlys <derlysr6@gmail.com>
2021-02-08 20:37:44 -0300

    usando função repeat do kotlin ao invez de usar um fori

--- FirstProject/src/Main.kt
+++ FirstProject/src/Main.kt
@@ -2,4 +2,5 @@ fun main(args: Array<String>) {
 
     val p1 = Piramide()
     p1.printedACollections()
+    p1.experimentsOfInternet()
 }
--- FirstProject/src/Piramide.kt
+++ FirstProject/src/Piramide.kt
@@ -1,10 +1,19 @@
 class Piramide {
+    private var inteiro = 1
     private val exampleCollection = arrayListOf(
             listOf(1, 3),
             listOf(3, 5),
             listOf("stranger"),
             listOf("null"))
+
     fun printedACollections() {
         for (listOf in exampleCollection) println(listOf)
+    }
+    fun experimentsOfInternet(){
+        repeat(23){
+
+            println("this is a example $inteiro")
+            inteiro++
+        }
     }
 }
